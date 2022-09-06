a8e8c5341d1cad25e9bf8c97d574c78ee9ee93c6
masahiro96848 <feetwcpic9436@gmail.com>
2022-09-07 08:59:09 +0900

    feat: ブログタイトルの表示のテストを作成

--- tests/Feature/Controllers/BlogViewControllerTest.php
+++ tests/Feature/Controllers/BlogViewControllerTest.php
@@ -2,18 +2,28 @@
 
 namespace Tests\Feature\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class BlogViewControllerTest extends TestCase
 {
+    use RefreshDatabase;
     /** @test index */
     function ブログのTopページを開ける()
     {
         // $this->withoutExceptionHandling();
+        $blog1 = Blog::factory()->create();
+        $blog2 = Blog::factory()->create();
+        $blog3 = Blog::factory()->create();
+
         $response = $this->get('/');
 
-        $response->assertOk();
+        $response->assertOk()
+        ->assertViewIs('index')
+                ->assertSee($blog1->title)
+                ->assertSee($blog2->title)
+                ->assertSee($blog3->title);
     }
 }
